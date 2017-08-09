712f696e3f4951017c545534b685d10ade24b3b6
maryito <mtorres@uip.edu.pa>
2017-08-08 23:58:04 -0500

    agregando la parte de C.R.U.D producto

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -6,14 +6,35 @@ import { Productos } from '../../api/productos/productos.js';
 Meteor.startup(() => {
 
   // Realizamos un carga inicial de datos a la colection de productos
-  // if( Productos.find().count() === 0 ){
-  //   data = [
-  //     { nombre: "computadora" },
-  //     { nombre: "Servidores" },
-  //     { nombre: "Camaras" },
-  //     { nombre: "Impresoras" }
-  //   ]
-  //   // Agregamos a la base de datos
-  //   data.forEach( producto => Productos.insert(producto))
-  // }
+  if (Productos.find().count() === 0) {
+    const data = [
+      {
+        nombre: 'Computadora',
+        cantidad: 100,
+        precio: 250,
+        proveedor: 'pv1',
+        marca: 'dell',
+        tiempo: 2,
+        createdAt: new Date(),
+      },
+      {
+        nombre: 'Computadora',
+        cantidad: 5000,
+        precio: 150,
+        proveedor: 'pv2',
+        marca: 'dell',
+        tiempo: 4,
+      },
+      {
+        nombre: 'Computadora',
+        cantidad: 1500,
+        precio: 90,
+        proveedor: 'pv3',
+        marca: 'dell',
+        tiempo: 1,
+      },
+    ];
+    // Agregamos a la base de datos
+    data.forEach(producto => Productos.insert(producto));
+  }
 });
